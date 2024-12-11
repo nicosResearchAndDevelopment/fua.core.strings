6cd48d54a993ab127aeb6755e6f20585e2d4dfc1
Simon Petrac <pfoerdie@gmail.com>
2024-12-11 17:26:10 +0100

    migration to github

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# @nrd/fua.core.strings
+# @fua/core.strings
--- test/strings.http.test.js
+++ test/strings.http.test.js
@@ -1,75 +1,75 @@
 const
-    {describe, test} = require('mocha'),
-    expect           = require('expect'),
-    http             = require('../src/strings.http.js');
+  { describe, test } = require('mocha'),
+  expect = require('expect'),
+  http = require('../src/strings.http.js');
 
 describe('fua.core.strings.http', function () {
 
-    describe('headers', function () {
+  describe('headers', function () {
 
-        const headers = http?.headers;
+    const headers = http?.headers;
 
-        describe('contentType', function () {
+    describe('contentType', function () {
 
-            const contentType = headers?.contentType;
+      const contentType = headers?.contentType;
 
-            describe('param', function () {
+      describe('param', function () {
 
-                const param = contentType?.param;
+        const param = contentType?.param;
 
-                test('test', function () {
-                    expect(param.test('Hello World!')).toBe(false);
-                    expect(param.test('charset=utf-8; boundary=something')).toBe(true);
-                    expect(param.test('charset=utf-8; ')).toBe(false);
-                    expect(param.test('  charset=utf-8 ')).toBe(false);
-                });
-
-                test('parse', function () {
-                    expect(
-                        param.parse('charset=utf-8; boundary=something; literal="Hello World!"; literal=""')
-                    ).toEqual({
-                        charset:  'utf-8',
-                        boundary: 'something',
-                        literal:  ['Hello World!', '']
-                    });
-                });
-
-            });
-
-            test('test', function () {
-                expect(contentType.test('Hello World!')).toBe(false);
-                expect(contentType.test('text/plain')).toBe(true);
-                expect(contentType.test('text/html; charset=utf-8; boundary=something')).toBe(true);
-                expect(contentType.test('text/html; charset=utf-8; ')).toBe(false);
-            });
-
-            test('parse', function () {
-                expect(
-                    contentType.parse('text/html; charset=utf-8; boundary=something; literal="Hello World!"; literal=""')
-                ).toEqual({
-                    format: 'text/html',
-                    param:  {
-                        charset:  'utf-8',
-                        boundary: 'something',
-                        literal:  ['Hello World!', '']
-                    }
-                });
-            });
-
+        test('test', function () {
+          expect(param.test('Hello World!')).toBe(false);
+          expect(param.test('charset=utf-8; boundary=something')).toBe(true);
+          expect(param.test('charset=utf-8; ')).toBe(false);
+          expect(param.test('  charset=utf-8 ')).toBe(false);
         });
 
-        describe('accept', function() {
-
-            const accept = headers?.accept;
-
-            test('match', function() {
-
-                accept.match('text/*, text/html; charset=utf-8; boundary=something , */* ; test=""');
-
-            });
-
+        test('parse', function () {
+          expect(
+            param.parse('charset=utf-8; boundary=something; literal="Hello World!"; literal=""')
+          ).toEqual({
+            charset: 'utf-8',
+            boundary: 'something',
+            literal: ['Hello World!', '']
+          });
         });
+
+      });
+
+      test('test', function () {
+        expect(contentType.test('Hello World!')).toBe(false);
+        expect(contentType.test('text/plain')).toBe(true);
+        expect(contentType.test('text/html; charset=utf-8; boundary=something')).toBe(true);
+        expect(contentType.test('text/html; charset=utf-8; ')).toBe(false);
+      });
+
+      test('parse', function () {
+        expect(
+          contentType.parse('text/html; charset=utf-8; boundary=something; literal="Hello World!"; literal=""')
+        ).toEqual({
+          format: 'text/html',
+          param: {
+            charset: 'utf-8',
+            boundary: 'something',
+            literal: ['Hello World!', '']
+          }
+        });
+      });
 
     });
+
+    describe('accept', function () {
+
+      const accept = headers?.accept;
+
+      test('match', function () {
+
+        // accept.match('text/*, text/html; charset=utf-8; boundary=something , */* ; test=""');
+
+      });
+
+    });
+
+  });
 
 });
